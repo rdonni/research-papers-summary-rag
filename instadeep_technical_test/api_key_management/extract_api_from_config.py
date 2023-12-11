e3079402dc1597a1e748a32a13ecc0353151d459
rdonni <rayane.donni@imt-atlantique.net>
2023-12-11 16:24:35 +0100

    Upate config_path

--- instadeep_technical_test/api_key_management/extract_api_from_config.py
+++ instadeep_technical_test/api_key_management/extract_api_from_config.py
@@ -1,7 +1,10 @@
 import configparser
+import os
+
+config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.ini")
 
 
-def get_api_key(config_path="../config.ini"):
+def get_api_key(config_path=config_path):
     print(config_path)
     config = configparser.ConfigParser()
     config.read(config_path)
